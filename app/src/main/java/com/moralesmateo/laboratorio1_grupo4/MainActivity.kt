ddb484e3037829666df85b4128e5a75a119cae9e
Mateo Morales <mateo.mora3320@gmail.com>
2026-09-14 20:22:28 -0500

    Initial Commit: Proyecto base estandarizado

---
+++ app/src/main/java/com/moralesmateo/laboratorio1_grupo4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.moralesmateo.laboratorio1_grupo4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moralesmateo.laboratorio1_grupo4.ui.theme.Laboratorio1_grupo4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio1_grupo4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio1_grupo4Theme {
+        Greeting("Android")
+    }
+}
